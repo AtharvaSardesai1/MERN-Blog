6d539a583aa572537d7d873b4db8719c0ecfede7
Atharva <atharvasardesai9@gmail.com>
2025-03-24 13:16:17 +0530

    sidebar added

--- client/src/compnents/DashSidebar.jsx
+++ client/src/compnents/DashSidebar.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
 import { Sidebar } from 'flowbite-react'
 import { HiUser, HiArrowSmRight } from 'react-icons/hi'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 export default function DashSidebar() {
     const location = useLocation();
+    const navigate = useNavigate();
     const [tab, setTab] = useState('');
+
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
         const tabFromUrl = urlParams.get('tab');
         if (tabFromUrl) {
-        setTab(tabFromUrl);
+            setTab(tabFromUrl);
         }
-   
-  }, [location.search]);
-  return (
-    <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-            <Sidebar.ItemGroup>
-                <Link to='/dashboard?tab=profile'>
-                    <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark'>
+    }, [location.search]);
+
+    const handleProfileClick = () => {
+        navigate('/dashboard?tab=profile');
+        setTab('profile');
+    };
+
+    return (
+        <Sidebar className='w-full md:w-56'>
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Sidebar.Item
+                        active={tab === 'profile'}
+                        icon={HiUser}
+                        label={'User'}
+                        labelColor='dark'
+                        onClick={handleProfileClick}
+                    >
                         Profile
                     </Sidebar.Item>
-                </Link>
-                <Sidebar.Item active icon={HiArrowSmRight} className='cursor-pointer'>
-                    Sign Out
-                </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
-    </Sidebar>
-  )
+                    <Sidebar.Item active icon={HiArrowSmRight} className='cursor-pointer'>
+                        Sign Out
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+    );
 }
